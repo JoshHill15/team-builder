2a7f94c2b9d04ce4771de3b028d63abd842ef60d
josh Hill <Josh_hill15@me.com>
2019-11-13 19:11:13 -0500

    mvp2

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,22 +1,22 @@
-import React, {useState} from 'react';
-import './App.css';
-import Form from "./Form"
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./Form";
 function App() {
-  const [teams, setTeams] = useState([])
-  const [edit, setEdit] = useState()
+  const [teams, setTeams] = useState([]);
+
   const addTeam = team => {
     const newTeam = {
       id: Date.now(),
       name: team.name,
       email: team.email,
       role: team.role
-    }
-    setTeams([...teams, newTeam])
-  }
+    };
+    setTeams([...teams, newTeam]);
+  };
   return (
     <div className="App-header">
       <h1>My Team</h1>
-      <Form addTeam = {addTeam} teams = {teams}/>
+      <Form addTeam={addTeam} teams={teams} />
     </div>
   );
 }
